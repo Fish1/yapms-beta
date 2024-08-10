bfeb99ac8d1a78b283cc30ada6e5f89f0de6d302
Jacob Enders <jacobenders1@gmail.com>
2024-08-09 21:51:39 -0400

    work

--- apps/yapms/src/lib/components/navbar/PresentationNavBar.svelte
+++ apps/yapms/src/lib/components/navbar/PresentationNavBar.svelte
@@ -1,15 +1,44 @@
 <script lang="ts">
 	import VideoCameraSlash from '$lib/icons/VideoCameraSlash.svelte';
 	import { PresentationModeStore } from '$lib/stores/PresentationMode';
+	import { afterUpdate, beforeUpdate, onMount } from 'svelte';
+	import LockButton from './buttons/LockButton.svelte';
+
+	let inside = false;
 
 	function togglePresentationMode() {
 		$PresentationModeStore.enabled = !$PresentationModeStore.enabled;
 	}
+
+	function toggleHover() {
+		inside = !inside;
+		console.log('TOGGLE');
+	}
+
+	onMount(() => {
+		console.log('mount');
+	});
+
+	beforeUpdate(() => {
+		console.log('before update', inside);
+	});
+
+	afterUpdate(() => {
+		console.log('after update', inside);
+	});
 </script>
 
-<button
-	class="btn btn-sm btn-error w-min m-1 absolute right-0 float-right z-10"
-	on:click={togglePresentationMode}
+<div
+	class="absolute right-0 m-1 z-10 flex flex-col gap-3 align-middle"
+	on:mouseenter={toggleHover}
+	on:mouseleave={toggleHover}
+	role="button"
+	tabindex={1}
 >
-	<VideoCameraSlash class="w-6 h-6" />
-</button>
+	<button class="btn btn-md btn-circle btn-error self-middle" on:click={togglePresentationMode}>
+		<VideoCameraSlash class="w-6 h-6" />
+	</button>
+	{#if inside}
+		<LockButton />
+	{/if}
+</div>
---
+++ apps/yapms/src/lib/components/navbar/buttons/LockButton.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import LockClosed from '$lib/icons/LockClosed.svelte';
+	import LockOpen from '$lib/icons/LockOpen.svelte';
+	import { LockMapStore } from '$lib/stores/LockMap';
+
+	function toggleMapLock() {
+		$LockMapStore = !$LockMapStore;
+	}
+</script>
+
+<button class="btn btn-md btn-neutral btn-circle self-middle" on:click={toggleMapLock}>
+	{#if $LockMapStore}
+		<LockClosed class="w-6 h-6" />
+	{:else}
+		<LockOpen class="w-6 h-6" />
+	{/if}
+</button>
